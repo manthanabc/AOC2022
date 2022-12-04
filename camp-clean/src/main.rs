8afb3b526df9918cc27cefe1943d7b6516e866ed
manthanabc <manthanmji@gmail.com>
2022-12-04 15:06:29 +0400

    day4

--- camp-clean/src/main.rs
+++ camp-clean/src/main.rs
@@ -1,7 +1,6 @@
 fn main() {
     let input = include_str!("../input.prod").lines();
-    let mut sum = 0;
-    input.for_each(|f| { let (x1, x2) = f.split_once(',').unwrap(); if contains(x1, x2) { sum += 1 };});
+    let mut sum = input.filter(|f| { let (x1, x2) = f.split_once(',').unwrap(); contains(x1, x2) }).count();
 
     println!("{sum}");
 }
@@ -14,9 +13,6 @@ fn contains(a: &str, b: &str) -> bool {
     let (b1, b2) = (b1.parse::<u16>().unwrap(), b2.parse::<u16>().unwrap());
 
 
-    //let s = (a1 <= b1 && a2 >= b2) || (b1 <= a1 && b2 >= a2);   // part 1
-    let s = (a1 <= b1 && a2 >= b1) || (b1 <= a1 && b2 >= a1);     // part 2
-
-    println!("{a1}-{a2} {b1}-{b2}  {s}");
-    s
+   // (a1 <= b1 && a2 >= b2) || (b1 <= a1 && b2 >= a2)   // part 1
+    (a1 <= b1 && a2 >= b1) || (b1 <= a1 && b2 >= a1)     // part 2
 }
