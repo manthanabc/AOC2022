da2ed1f45255ee8e4c784379d2430f93d550c4be
manthanabc <manthanmji@gmail.com>
2022-12-06 10:51:38 +0400

    clean up for day 6

--- tuning-trouble/src/main.rs
+++ tuning-trouble/src/main.rs
@@ -2,19 +2,16 @@ fn main() {
     let mut input = include_str!("../input.prod").lines();
     let mut acc = vec![];
     input.next().unwrap().chars().enumerate().fold(acc, |mut acc, (n, a)| {
-        println!("start of {n} acc {:?}, a {a}", acc);
+//        println!("start of {n} acc {:?}, a {a}", acc);
         if(acc.len() > 13) {
-            acc.remove(0);
-            if !acc.contains(&a) {dbg!(n); panic!();}
+            dbg!(n); panic!();
         }
         if acc.contains(&a) {
                 let p = acc.iter().position(|g| *g == a).unwrap();
                 acc=acc.split_off(p+1);
         }
         acc.push(a);
-        if(acc.len() == 14) {
-            dbg!(n+1); panic!();
-        }
+//        println!("acc {:?}, a {a} \n\n", acc);
         acc
     });
 }
