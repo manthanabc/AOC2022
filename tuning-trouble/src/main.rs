f5af1eaa8d48d4c19e67402125e55b3b0a2fab53
manthanabc <manthanmji@gmail.com>
2022-12-07 19:40:49 +0400

    part 1 day 7

--- tuning-trouble/src/main.rs
+++ tuning-trouble/src/main.rs
@@ -1,10 +1,14 @@
+
+use std::time::Instant;
 fn main() {
     let mut input = include_str!("../input.prod").lines();
+    let now = Instant::now();
     let mut acc = vec![];
     input.next().unwrap().chars().enumerate().fold(acc, |mut acc, (n, a)| {
 //        println!("start of {n} acc {:?}, a {a}", acc);
         if(acc.len() > 13) {
-            dbg!(n); panic!();
+            dbg!(n);        dbg!(now.elapsed());
+ panic!();
         }
         if acc.contains(&a) {
                 let p = acc.iter().position(|g| *g == a).unwrap();
