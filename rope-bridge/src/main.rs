b8d53e83c5b76a5f4d3521674b80dac5ebaa591e
manthanabc <manthanmji@gmail.com>
2022-12-09 14:39:48 +0400

    day 9

--- rope-bridge/src/main.rs
+++ rope-bridge/src/main.rs
@@ -1,34 +1,49 @@
-#[derive(Debug)]
+use std::collections::HashSet;
+
+#[derive(Debug, Eq, Hash, PartialEq, Clone)]
 struct position(i32, i32);
 
 fn main() {
-    let input = include_str!("../input.test");
-    let mut head = position(0, 0);
-    let mut tail = position(0, 0);
+    let input = include_str!("../input.prod");
+    let mut nodes = vec![position(0, 0); 10];
+    let mut counter = 0;
+
+    let mut map = HashSet::new();
+
     input.lines().for_each(|line| {
-        match line.split_once(" ").unwrap() {
-            ("R", x) => head.0 += x.parse::<u32>().unwrap(),
-            ("L", x) => head.0 -= x.parse::<u32>().unwrap(),
-            ("U", x) => head.1 += x.parse::<u32>().unwrap(),
-            ("D", x) => head.1 -= x.parse::<u32>().unwrap(),
+        let (mut dir, x) = match line.split_once(" ").unwrap() {
+            ("R", x) => (position( 1, 0 ), x),
+            ("L", x) => (position(-1, 0 ), x),
+            ("U", x) => (position( 0, 1 ), x),
+            ("D", x) => (position( 0, -1), x),
              _       => panic!("PANKID")
-        }
-        println!("{line} {:?}", head);
+        };
+        movee(&mut nodes, &mut dir, x.parse().unwrap(), &mut counter, &mut map);
+        // println!("{line} {:?}", nodes);
     });
-    println!("Hello, world!");
+    println!("Hello, world! {counter}");
 }
 
-fn movee(head: &mut position, tail: &mut position, dir: &position, n: u32) {
+fn movee(nodes: &mut Vec<position>, dir: &position, n: u32, counter: &mut i32, map: &mut HashSet<position>) {
     for i in 0..n {
-        *head = position(head.0 + dir.0, head.0 + dir.0);
-        move_tail(tail, head);
+        nodes[0] = position(nodes[0].0 + dir.0, nodes[0].1 + dir.1);
+        for i in 1..nodes.len() {
+            let nd = &nodes[i-1].clone();
+            move_tail(&mut nodes[i], nd);
+        }
+        if !map.contains(&nodes[nodes.len()-1]) {
+            *counter += 1;
+            map.insert(nodes[nodes.len()-1].clone());
+        }
     }
 }
 
 fn move_tail(tail: &mut position, head: &position) {
-    if (tail.0 - head.0) > 1 {
-        tail.0 = if tail.0 > head.0 { head.0+1 } else { head.0-1 }
-    } else if (tail.0 - head.0) > 1 {
-        tail.0 = if tail.0 > head.0 { head.0+1 } else { head.0-1 }
+    if (tail.0 - head.0).abs() > 1 {
+        tail.0 = if tail.0 > head.0 { tail.0-1 } else if tail.0 < head.0 { tail.0+1 } else { tail.0 };
+        tail.1 = if tail.1 > head.1 { tail.1-1 } else if tail.1 < head.1 { tail.1+1 } else { tail.1 };
+    } else if (tail.1 - head.1).abs() > 1 {
+        tail.1 = if tail.1 > head.1 { tail.1-1 } else if tail.1 < head.1 { tail.1+1 } else { tail.1 };
+        tail.0 = if tail.0 > head.0 { tail.0-1 } else if tail.0 < head.0 { tail.0+1 } else { tail.0 };
     }
 }
