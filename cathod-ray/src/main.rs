ca61ec23a1a84f354361f654ac7221ea20a960d7
manthanabc <manthanmji@gmail.com>
2022-12-10 11:24:13 +0400

    reformatting day 10

--- cathod-ray/src/main.rs
+++ cathod-ray/src/main.rs
@@ -1,4 +1,3 @@
-
 fn main() {
     let input = include_str!("../input.prod");
     let mut x: i32 = 1;
@@ -12,70 +11,39 @@ fn main() {
         }
         let mut line = line.split(" ");
         let isnt = line.next().unwrap();
-        let pra  = line.next();
-        /* (0..if isnt=="addx" {2} else {0}).for_each(|_| {
-            
-            cycle += 1
-        }); */
-       
+        let pra = line.next();
+
         match isnt {
             "addx" => {
-                /* if (cycle -20) %40 == 0 { println!("----{cycle} {x} {i}");
-                    sum+=x*cycle;
-                } */
-                if (cycle%40-x).abs() <= 1 {
-                    let index: usize = (cycle%40).try_into().unwrap();
-                    position[index] = "#";        
-                }
-                cycle+=1;
-                if (cycle%40-x).abs() <= 1 {
-                    let index: usize = (cycle%40).try_into().unwrap();
-                    position[index] = "#";        
-                }
-                if (cycle )% 40 == 0 { // println!("----{cycle} {x} {i}");
-                    print(&mut position);
-                    // println!("{x}");
-                    position = vec!["."; 40];
-                    sum+=x*cycle;
-                }
-                cycle+=1;
-                if (cycle )% 40 == 0 { // println!("----{cycle} {x} {i}");
-                    print(&mut position);
-                    // println!("{x}");
-                    position = vec!["."; 40];
-
-                    sum+=x*cycle;
-                } 
-                x+= pra.unwrap().parse::<i32>().unwrap();
-                continue;
-            },
-            _   => ()
+                tick(&mut position, &mut cycle, &mut sum, x);
+                tick(&mut position, &mut cycle, &mut sum, x);
+                x += pra.unwrap().parse::<i32>().unwrap();
+            }
+            _ => tick(&mut position, &mut cycle, &mut sum, x),
         };
-                if (cycle%40-x).abs() <= 1 {
-                    let index: usize = (cycle%40).try_into().unwrap();
-                    position[index] = "#";        
-                }
-        cycle += 1;
-        if (cycle )% 40 == 0 {
-            //println!("{:?}", position);
-                    print(&mut position);
-            position = vec!["."; 40];
-            // println!("--------------------{cycle} {x} {:?}", pra);
-            sum += x*cycle;
-        } //if (cycle -20)% 40 == 0 { sum+=x*cycle };
-            /*            if (cycle -20)% 40 == 0 {
-            println!("--------------------{cycle} {x} {:?}", pra);
-            sum += x*cycle;
-        } */
-        // println!("{} {x}{:?}", isnt, pra);
     }
-    println!("Hello, world! {sum}");
+    println!("Sum: {sum}");
 }
 
-fn print(veec:&mut Vec<&str>) {
-        let mut it =veec.iter();
-        while let Some(vec) =  it.next() {
-            print!("{vec}");
-        }
-        println!();
+fn tick(position: &mut Vec<&str>, cycle: &mut i32, sum: &mut i32, x: i32) {
+    if (*cycle % 40 - x).abs() <= 1 {
+        let index: usize = (*cycle % 40).try_into().unwrap();
+        position[index] = "#";
+    }
+    *cycle += 1;
+    if (*cycle) % 40 == 0 {
+        print(position);
+        *position = vec!["."; 40];
+    }
+    if (*cycle -20) %40 == 0 {
+        *sum+=x* *cycle;
+    }
+}
+
+fn print(veec: &mut Vec<&str>) {
+    let mut it = veec.iter();
+    while let Some(vec) = it.next() {
+        print!("{vec}");
+    }
+    println!();
 }
